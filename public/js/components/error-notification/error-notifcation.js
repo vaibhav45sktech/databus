dc677e01e15679ad626cf8490107bbd02eda942d
vaibhav45sktech <vm872108@gmail.com>
2025-12-06 21:01:17 +0530

    feat: customize 401 Unauthorized page with login prompt

--- public/js/components/error-notification/error-notifcation.js
+++ public/js/components/error-notification/error-notifcation.js
@@ -3,6 +3,8 @@ const { DatabusMsg } = require("../../utils/messages");
 function ErrorNotificationController() {
   var ctrl = this;
   ctrl.expanded = false;
+  ctrl.code = $ctrl.key;
+
 
   ctrl.toggleExpand = function () {
     ctrl.expanded = !ctrl.expanded;
